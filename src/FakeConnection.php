77d188b9b9edb147e0992ce02c29168b44582881
xalaida <nevadskiy@gmail.com>
2024-09-29 21:05:37 +0300

    handle inserts

--- src/FakeConnection.php
+++ src/FakeConnection.php
@@ -43,7 +43,23 @@ class FakeConnection extends Connection
             throw new RuntimeException(sprintf('Unexpected select query bindings: [%s] [%s]', $query, implode(', ', $bindings)));
         }
 
-        throw new RuntimeException(sprintf('Unexpected select query: [%s]', $query));
+        throw new RuntimeException(sprintf('Unexpected select query: [%s] [%s]', $query, implode(', ', $bindings)));
+    }
+
+    #[Override]
+    public function insert($query, $bindings = [])
+    {
+        $queryExpectation = array_shift($this->queryExpectations);
+
+        if ($queryExpectation && $queryExpectation->sql === $query) {
+            if ($this->compareBindings($queryExpectation->bindings, $bindings)) {
+                return $queryExpectation->successfulStatement;
+            }
+
+            throw new RuntimeException(sprintf('Unexpected insert query bindings: [%s] [%s]', $query, implode(', ', $bindings)));
+        }
+
+        throw new RuntimeException(sprintf('Unexpected insert query: [%s] [%s]', $query, implode(', ', $bindings)));
     }
 
     protected function compareBindings(array | null $expectedBindings, array $actualBindings): bool
