4a51dce71bc8af23e950b3cade0384d2317c92c5
xalaida <nevadskiy@gmail.com>
2024-10-06 20:44:47 +0300

    split transaction tests into different test cases

--- tests/TransactionTest.php
+++ tests/TransactionTest.php
@@ -196,46 +196,4 @@ class TransactionTest extends TestCase
 
         $connection->assertExpectationsFulfilled();
     }
-
-    #[Test]
-    public function itShouldVerifyTransactionAfterExecution(): void
-    {
-        $connection = $this->getFakeConnection();
-
-        // TODO: refactor this configuration using single method to handle all write queries automatically
-        $connection->recordTransactions();
-        $connection->onInsertQuery(fn () => 1);
-
-        $connection->transaction(function () use ($connection) {
-            (new Builder($connection))
-                ->from('users')
-                ->insert(['name' => 'xala']);
-        });
-
-        $connection->assertBeganTransaction();
-
-        $connection->assertQueried('insert into "users" ("name") values (?)', ['xala']);
-
-        $connection->assertCommitted();
-    }
-
-    #[Test]
-    public function itShouldVerifyTransactionAfterExecutionUsingCallableSyntax(): void
-    {
-        $connection = $this->getFakeConnection();
-
-        // TODO: refactor this configuration using single method to handle all write queries automatically
-        $connection->recordTransactions();
-        $connection->onInsertQuery(fn () => 1);
-
-        $connection->transaction(function () use ($connection) {
-            (new Builder($connection))
-                ->from('users')
-                ->insert(['name' => 'xala']);
-        });
-
-        $connection->assertTransaction(function () use ($connection) {
-            $connection->assertQueried('insert into "users" ("name") values (?)', ['xala']);
-        });
-    }
 }
