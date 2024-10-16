714843b6568ee0a84c8e0c1a250fcc62e7d012e0
xalaida <nevadskiy@gmail.com>
2024-10-16 20:10:45 +0300

    support scalar values

---
+++ tests/ScalarQueryTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Xala\Elomock;
+
+use PHPUnit\Framework\Attributes\Test;
+
+class ScalarQueryTest extends TestCase
+{
+    #[Test]
+    public function itShouldHandleScalarQuery(): void
+    {
+        $connection = $this->getFakeConnection();
+
+        $connection->expectQuery('select count(*) from "users"')
+            ->andReturnRows([
+                ['count' => 5],
+            ]);
+
+        $value = $connection->scalar('select count(*) from "users"');
+
+        static::assertEquals(5, $value);
+    }
+}
