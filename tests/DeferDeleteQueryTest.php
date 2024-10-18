c54b09ea25e7a544cd0f7787b8deca6d2df64da5
xalaida <nevadskiy@gmail.com>
2024-10-19 01:13:09 +0300

    rename method

--- tests/DeferDeleteQueryTest.php
+++ tests/DeferDeleteQueryTest.php
@@ -92,6 +92,6 @@ class DeferDeleteQueryTest extends TestCase
         $this->expectException(ExpectationFailedException::class);
         $this->expectExceptionMessage('Some write queries were not fulfilled');
 
-        $connection->assertDeferredQueriesFulfilled();
+        $connection->assertDeferredQueriesVerified();
     }
 }
