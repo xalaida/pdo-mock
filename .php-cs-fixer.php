e12b58b16c7f31ab110b606012015cfd8fc25985
kalaida <nevadskiy@gmail.com>
2024-11-10 22:31:38 +0200

    configure visibility_required for php5.6

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,7 +8,13 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PER-CS' => true,
         'trailing_comma_in_multiline' => false,
-        'braces_position' => false
+        'braces_position' => false,
+        'visibility_required' => [
+            'elements' => [
+                'property',
+                'method'
+            ]
+        ]
     ])
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setCacheFile(__DIR__ . '/var/php-cs-fixer.cache')
